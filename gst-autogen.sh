ae138c8944d9ac7cb076a914afc5ce71c3114e39
Thomas Vander Stichele <thomas@apestaart.org>
2002-04-16 14:07:45 +0000

    make autoconf check listen to --nocheck
    Original commit message from CVS:
    make autoconf check listen to --nocheck

--- gst-autogen.sh
+++ gst-autogen.sh
@@ -110,13 +110,16 @@ autoconf_2.52d_check ()
 {
   # autoconf 2.52d has a weird issue involving a yes:no error
   # so don't allow it's use
-  ac_version=`$autoconf --version|head -n 1|sed 's/^[a-zA-z\.\ ()]*//;s/ .*$//'`
-  if test "$ac_version" = "2.52d"; then
-    echo "autoconf 2.52d has an issue with our current build."
-    echo "We don't know who's to blame however.  So until we do, get a"
-    echo "regular version.  RPM's of a working version are on the gstreamer site."
-    exit 1
-  fi
+  test -z "$NOCHECK" && {
+    ac_version=`$autoconf --version|head -n 1|sed 's/^[a-zA-z\.\ ()]*//;s/ .*$//'`
+    if test "$ac_version" = "2.52d"; then
+      echo "autoconf 2.52d has an issue with our current build."
+      echo "We don't know who's to blame however.  So until we do, get a"
+      echo "regular version.  RPM's of a working version are on the gstreamer site."
+      exit 1
+    fi
+  }
+  return 0
 }
 
 die_check ()
