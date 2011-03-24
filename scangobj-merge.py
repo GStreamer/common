d6e3fe913375e82a83fbc3fbdf8b26b8ce8bf97c
Stefan Kost <ensonic@users.sf.net>
2011-03-24 11:35:09 +0200

    spelling: fix spelling in comment and message

--- scangobj-merge.py
+++ scangobj-merge.py
@@ -261,7 +261,7 @@ def main(argv):
     try:
         modulename = argv[1]
     except IndexError:
-        sys.stderr.write('Pleae provide a documentation module name\n')
+        sys.stderr.write('Please provide a documentation module name\n')
         sys.exit(1)
 
     print "Merging scangobj output for %s" % modulename
